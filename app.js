6e5b2e7e2b53bbce73b294f84b1ccd3940043656
Haobo Wang <wangh518@mcmaster.ca>
2026-02-27 14:12:35 -0500

    Initialize sqlite database and add select by id route

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const path = require('path');
 
+const appointmentsModel = require('./models/appointments.model')
+appointmentsModel.init()
+
 const app = express();
 
 // Mustache setup
@@ -15,6 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const appointmentsController = require('./controllers/appointments.ctrl');
 
 app.get('/', appointmentsController.showHome);
+app.get('/appointments/:id', appointmentsController.showById)
 
 const PORT = 3000;
 app.listen(PORT, () => {
