c59329ec4ba33a765bf3b3057aab95eb6275e07e
Haobo Wang <wangh518@mcmaster.ca>
2026-02-27 13:04:32 -0500

    Load appointment list and selected details from sqlite

--- controllers/appointments.ctrl.js
+++ controllers/appointments.ctrl.js
@@ -1,15 +1,44 @@
-const appointmentsModel = require('../models/appointments.model');
+const appointmentsModel = require('../models/appointments.model')
 
 function showHome(req, res) {
-    const data = appointmentsModel.getSampleAppointments();
+    appointmentsModel.getAll((err, rows) => {
+        if (err) {
+            return res.status(500).send('Database error')
+        }
 
-    res.render('index', {
-        pageTitle: 'Simple Appointment Planner',
-        appointments: data.appointments,
-        selected: data.selected
-    });
+        const selected = rows.length > 0 ? rows[0] : null
+
+        res.render('index', {
+            pageTitle: 'Simple Appointment Planner',
+            appointments: rows,
+            selected
+        })
+    })
+}
+
+function showById(req, res) {
+    const id = Number(req.params.id)
+
+    appointmentsModel.getAll((err, rows) => {
+        if (err) {
+            return res.status(500).send('Database error')
+        }
+
+        appointmentsModel.getById(id, (err2, selected) => {
+            if (err2) {
+                return res.status(500).send('Database error')
+            }
+
+            res.render('index', {
+                pageTitle: 'Simple Appointment Planner',
+                appointments: rows,
+                selected
+            })
+        })
+    })
 }
 
 module.exports = {
-    showHome
-};
+    showHome,
+    showById
+}
